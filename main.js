ab132e7431d4452882a27e54206c565fe1775a8a
Nicolly <163870478+nicgiroldo@users.noreply.github.com>
2025-04-16 09:37:21 -0300

    nhhhhhh

--- main.js
+++ main.js
@@ -16,10 +16,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-04-26T19:30:00");
+const tempoObjetivo1 = new Date("2025-09-12T19:30:00");
 const tempoObjetivo2 = new Date("2025-12-30T00:00:00");
-const tempoObjetivo3 = new Date("2026-03-10T00:00:00");
-const tempoObjetivo4 = new Date("2026-01-01T00:00:00");
+const tempoObjetivo3 = new Date("2026-01-01T00:00:00");
+const tempoObjetivo4 = new Date("2026-03-10T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
